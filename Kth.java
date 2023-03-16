ffa1d0b130bd35647a3143548570b7716c3075ce
Badisa Sai Santosh <96568872+BadisaSaiSantosh@users.noreply.github.com>
2023-03-16 14:54:41 +0530

    Add files via upload

---
+++ Kth.java
@@ -0,0 +1,44 @@
+//K largest elements from a big file or array.
+
+
+
+import java.util.*;
+class Kth{
+import java.util.*;
+class HelloWorld {
+   
+ public static void main(String[] args) {
+  
+     Scanner sc=new Scanner(System.in);
+    
+     int k=sc.nextInt();
+      
+     int Arr[]={63,34,8,90,233,67,34,23};
+    
+     Arrays.sort(Arr);
+       
+      
+  for(int i=0;i<k;i++){
+      
+      // For largest kth elements
+   
+      System.out.print(Arr[(Arr.length)-1-i]+",");
+   
+      // For smallest kth elements
+      
+      // System.out.print(Arr[i]);
+     
+       }
+      
+ 
+   }
+
+}
+
+
+// expected output
+
+// 4
+
+// 233,90,67,63,
+
